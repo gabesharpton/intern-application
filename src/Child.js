09207f7d7aa33f679df4c4581b7c63269ecf8c70
gabesharpton <gabriel.sharpton@gmail.com>
2020-05-06 18:47:13 -0500

    pulling props color into children background color, and added alternating words depending on the blink props

--- src/Child.js
+++ src/Child.js
@@ -1,6 +1,30 @@
-import React from 'react'
-function Child() {
-  return <h1>Hello World</h1>;
+import React, { useState } from "react";
+
+function Child(props) {
+  const [color, setColor] = useState(props);
+
+  function changeColor() {
+    setColor(`${props.color}`);
+  }
+
+  return (
+    <div className="full" style={{ "background-color": `${props.color}` }}>
+      <div className="hw" style={{ color: `black` }} onClick={changeColor}>
+        Hello World
+      </div>
+
+      {props.blink ? (
+        <h2 className="blink" style={{ color: `black` }}>
+          Hello
+        </h2>
+      ) : (
+        <h2 className="blink" style={{ color: `black` }}>
+          World
+        </h2>
+      )}
+    </div>
+  );
 }
 
-export default Child
+console.log(Child);
+export default Child;
